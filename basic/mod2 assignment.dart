d88cbc99775ff770b4dece3082c906e1e2ea8983
USER <user@abrars-macbook-air.local>
2023-08-04 04:48:41 +0600

    Initial commit

---
+++ basic/mod2 assignment.dart
@@ -0,0 +1,21 @@
+
+void main() {
+  List <int> given_list = [85, 92, 78, 65, 88, 72];
+  int sum = 0, count = 0, mark = 0;
+  double avg = 0;
+  for (int e in given_list){
+    sum = sum + e;
+    count = count + 1;
+  }
+  avg = sum / count;
+  print("Student's average grade: $avg");
+  mark = avg.round();
+  print("Rounded average: $mark");
+  if (mark >= 70){
+    print("Passed");
+  }
+  else {
+    print("Failed");
+  }
+
+}
