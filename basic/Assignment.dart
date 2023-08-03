f6bc4665a598a2b7caa23ad3aa2c5978b55e9a11
USER <user@Abrars-Macbook-air.local>
2023-08-04 04:48:41 +0600

    Initial commit

---
+++ basic/Assignment.dart
@@ -0,0 +1,17 @@
+void main(){
+  const int a = 7;
+  const int b = 3;
+  print("a = $a");
+  print("b = $b");
+  var add = a + b;
+  var sub = a - b;
+  var mul = a * b;
+  var div = a / b;
+  var mod = a % b;
+  print("Addition = $add");
+  print("Subtraction = $sub");
+  print("Multiplication = $mul");
+  print("Division = $div");
+  print("Modulus = $mod");
+
+}
